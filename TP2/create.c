2717edaeab6582827a812dc42a09a1fbe8d574bb
Matias Pierri <matias.p@marvik.ai>
2023-09-03 00:42:43 -0300

    create.c

---
+++ TP2/create.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* f1(void *x){
+  int j = *((int*) x);
+  sleep(1);
+  printf("f1: %d\n",j);
+  pthread_exit(0);
+}
+
+void* f2(void *x){
+  int j = *((int*) x);
+  sleep(1);
+  printf("f2: %d\n",j);
+  pthread_exit(0);
+}
+
+int main()  {
+  pthread_t f2_thread, f1_thread;
+  int i1,i2;
+  i1 = 1;
+  i2 = 2;
+  pthread_create(&f1_thread,NULL,f1, &i1);
+  pthread_create(&f2_thread,NULL,f2, &i2);
+  return(0);
+}
