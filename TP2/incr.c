180abfea18971fc264bad62ec2190058d6ae5f4d
Matias Pierri <matias.p@marvik.ai>
2023-09-03 02:05:23 -0300

    time

--- TP2/incr.c
+++ TP2/incr.c
@@ -4,6 +4,7 @@
  */
 #include <pthread.h>
 #include <stdio.h>
+#include <time.h>
 #define THREADS 2
 #define K 10 * 1000 * 1000
 
@@ -34,9 +35,9 @@ void* threadfunc(void* arg) {
     int thread_num=* (int*) arg;
     printf("este es el hilo %d\n", thread_num);
     for (int i = 0; i < K; ++i) {
-        // enter_region(thread_num);
+        enter_region(thread_num);
         x++;  // se incrementa la variable global
-        // leave_region(thread_num);
+        leave_region(thread_num);
     }
     printf("hilo %d finalizando\n", thread_num);
     return 0;
@@ -54,4 +55,26 @@ int main() {
 
 
     printf("x=%u\n", x);
+
+    clock_t start_clock, end_clock;
+    double cpu_time_used_clock;
+
+    start_clock = clock();
+    performTask(); // Call the function whose execution time you want to measure
+    end_clock = clock();
+
+    cpu_time_used_clock = ((double)(end_clock - start_clock)) / CLOCKS_PER_SEC;
+    printf("CPU time used (clock()): %f seconds\n", cpu_time_used_clock);
+
+    // Method 2: Using gettimeofday()
+    struct timeval start_timeval, end_timeval;
+    double elapsed_timeval;
+
+    gettimeofday(&start_timeval, NULL);
+    performTask(); // Call the function whose execution time you want to measure
+    gettimeofday(&end_timeval, NULL);
+
+    elapsed_timeval = (end_timeval.tv_sec - start_timeval.tv_sec) +
+                      (end_timeval.tv_usec - start_timeval.tv_usec) / 1000000.0;
+    printf("Elapsed time (gettimeofday()): %f seconds\n", elapsed_timeval);
 }
